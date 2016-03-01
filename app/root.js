7376d2b0bdb0c08b1717836ff5fa3082165d5d5d
Lisa <yushihe@g.ucla.edu>
2016-03-01 13:43:07 -0800

    Go to feed!

--- app/root.js
+++ app/root.js
@@ -9,12 +9,65 @@ import React, {
   ListView,
   DatePickerIOS,
   Date,
+  Navigator,
+  ScrollView,
+  TouchableHighlight,
 } from 'react-native';
+
 
 var REQUEST_URL = 'http://dev.bruinmobile.com/restaurants.json';
 
+class FeedView extends Component {
+	onPressFeed() {
+        this.props.navigator.push({
+            name: 'WelcomeView',
+            component: WelcomeView
+        });
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text onPress={this.onPressFeed.bind(this)}>
+                    Feed View!
+                </Text>
+            </View>
+        );
+    }
+}
+
 class Root extends Component {
-    constructor(props) {
+	 render() {
+        return (
+            <Navigator
+                initialRoute={{name: 'WelcomeView', component: WelcomeView}}
+                configureScene={() => {
+                    return Navigator.SceneConfigs.FloatFromRight;
+                }}
+                renderScene={(route, navigator) => {
+                    // count the number of func calls
+                    console.log(route, navigator); 
+
+                    if (route.component) {
+                        return React.createElement(route.component, { navigator });
+                    }
+                }}/>
+        );
+    }
+
+
+
+
+}
+
+class WelcomeView extends React.Component {
+    onPressFeed() {
+        this.props.navigator.push({
+            name: 'FeedView',
+            component: FeedView
+        });
+    }
+
+        constructor(props) {
     super(props);
     this.state = {
       dataSource: new ListView.DataSource({
@@ -46,10 +99,16 @@ class Root extends Component {
     }
  
     return (
+    	<View style={styles.container}>
       <ListView
         dataSource={this.state.dataSource}
         renderRow={this.renderRestaurant}
         style={styles.listView}/>
+        
+        	<Text onPress={this.onPressFeed.bind(this)}>
+                    Go to feed!
+                </Text>
+            </View>
     );
   }
  
@@ -70,14 +129,38 @@ class Root extends Component {
 
         <View style={styles.rightContainer}>
           <Text style={styles.title}>{myrestaurant.Name}</Text>
-          <Text style={styles.year}>{myrestaurant.Address}</Text>
+          <Text style={styles.year}>
+          {myrestaurant.Address}</Text>
+
         </View>
       </View>
     );
   }
+/*
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome View!
+                </Text>
+
+                <Text onPress={this.onPressFeed.bind(this)}>
+                    Go to feed!
+                </Text>
+            </View>
+        );
+    }
+    */
 }
- 
+
 var styles = StyleSheet.create({
+	messageText: {
+    fontSize: 17,
+    fontWeight: '500',
+    padding: 15,
+    marginTop: 50,
+    marginLeft: 15,
+  },
   container: {
     flex: 1,
     flexDirection: 'row',
@@ -109,9 +192,25 @@ var styles = StyleSheet.create({
     paddingTop: 20,
     backgroundColor: '#F5FCFF',
   },
+  button: {
+    backgroundColor: 'white',
+    padding: 15,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: '#CDCDCD',
+  },
+  buttonText: {
+    fontSize: 17,
+    fontWeight: '500',
+  },
+  scene: {
+    flex: 1,
+    paddingTop: 20,
+    backgroundColor: '#EAEAEA',
+  }
 });
 
 
 AppRegistry.registerComponent('LoyaltyApp', () => LoyaltyApp);
 
 module.exports = Root;
+
